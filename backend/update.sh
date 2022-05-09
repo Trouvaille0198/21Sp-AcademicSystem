f9dc480b308314b78f9b3bdaa2162e52db508a12
MelonSoul <1375911011@qq.com>
2022-05-10 01:35:02 +0800

    fix: 解决 ssh 连接无法识别 go 指令的问题

--- backend/update.sh
+++ backend/update.sh
@@ -5,7 +5,8 @@ git pull
 cd ~/Repo/21Sp-AcademicSystem/backend
 rm -r nohup.out
 rm -r ./academic-system
-go mod tidy
-swag init
-go build
+# 也许是non-login模式下, ssh登录识别不了环境变量, 所以只能写全路径
+/opt/go/bin/go mod tidy
+/opt/gocode/bin/swag init
+/opt/go/bin/go build
 nohup ./academic-system >./nohup.out 2>&1 &
