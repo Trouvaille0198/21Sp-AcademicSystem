403007e5f6b9f12cbf6cd21b0edb272cefa1ec88
Amadeus075 <Amadeus075@outlook.com>
2022-05-15 23:39:39 +0800

    5.15  初步完成学生的课表查询、选课查询页面

--- frontend/src/views/Student/courseGrade/queryCourseGrade.vue
+++ frontend/src/views/Student/courseGrade/queryCourseGrade.vue
@@ -2,7 +2,7 @@
   <div>
     <el-form >
       <el-form-item label="选择学期">
-        <el-select v-model="term" placeholder="请选择学期">
+        <el-select v-model="term" @change="currTermChange" placeholder="请选择学期">
           <el-option v-for="(item, index) in termList" :key="index" :label="item" :value="item"></el-option>
         </el-select>
       </el-form-item>
@@ -14,32 +14,32 @@
           style="width: 100%">
         <el-table-column
             fixed
-            prop="cid"
-            label="课号"
-            width="150">
-        </el-table-column>
-        <el-table-column
-            prop="cname"
+            prop="number"
             label="课程号"
             width="150">
         </el-table-column>
         <el-table-column
-            prop="tid"
-            label="教师号"
+            prop="name"
+            label="课程名"
             width="150">
         </el-table-column>
         <el-table-column
-            prop="tname"
-            label="教师名称"
+            prop="department"
+            label="学院"
             width="150">
         </el-table-column>
         <el-table-column
-            prop="ccredit"
+            prop="teacher_name"
+            label="教师名"
+            width="150">
+        </el-table-column>
+        <el-table-column
+            prop="credit"
             label="学分"
             width="150">
         </el-table-column>
         <el-table-column
-            prop="grade"
+            prop="score"
             label="成绩"
             width="150">
         </el-table-column>
@@ -70,41 +70,73 @@ export default {
       let ans = (end < length) ? end : length
       that.tableData = that.tmpList.slice(start, ans)
     },
+
+    currTermChange(changeTerm) {
+      console.log("method中更改了selectedTerm：" + changeTerm)  // 测试
+      sessionStorage.setItem("selectedTerm", changeTerm)
+      
+    },
   },
   data() {
     return {
       tableData: null,
       pageSize: 10,
       total: null,
-      tmpList: null,
+      tmpList: [],
       avg: 0,
-      term: sessionStorage.getItem('currentTerm'),
+      term: sessionStorage.getItem('selectedTerm'),
       termList: null
     }
   },
   created() {
     const that = this
-    axios.get('http://localhost:10086/SCT/findAllTerm').then(function (resp) {
-      that.termList = resp.data
-    })
+    that.termList = ["21-秋季学期", "21-冬季学期", "21-春季学期", "22-秋季学期", "22-冬季学期", "22-春季学期"] 
   },
+
   watch: {
     term: {
       handler(newTerm, oldTerm) {
-        const sid = sessionStorage.getItem('sid')
+        const snumber = sessionStorage.getItem('number')
+        const sid = sessionStorage.getItem('id')
+        console.log("测试snumber：" + snumber)  // 测试
+        console.log("测试sid：" + sid)  // 测试
         const that = this
-        axios.get('http://localhost:10086/SCT/findBySid/' + sid + '/' + newTerm).then(function (resp) {
-          that.tmpList = resp.data
-          that.total = resp.data.length
+
+        // const term = sessionStorage.getItem('currentTerm')
+        let selectedTerm = sessionStorage.getItem('selectedTerm')
+        console.log("测试selectedTerm：" + selectedTerm)  // 测试
+      
+        // console.log("测试term：" + term)  // 测试
+        
+        this.tmpList = []
+        axios.get('http://1.15.130.83:8080/api/v1/student/' + sid + '/selected_course?hasScore=true').then(function (resp) {
+          
+          for (let i = 0; i < resp.data.data.length; i++) {
+            if (resp.data.data[i].term === selectedTerm) {
+              console.log("测试resp.data.data[i].term：" + resp.data.data[i].term + "    ,i:" + i)  // 测试
+              that.tmpList.push(resp.data.data[i])
+              // console.log("第二次测试selectedTerm：" + selectedTerm)  // 测试
+            }
+          }
+
+
+          // that.tmpList = resp.data.data
+          // console.log("测试code：" + resp.data.code)  // 测试
+          that.total = that.tmpList.length
+          console.log("测试total：" + that.total)  // 测试
           let start = 0, end = that.pageSize
           let length = that.tmpList.length
+          console.log("测试课程的数量length：" + length)  // 测试
           let ans = (end < length) ? end : length
           that.tableData = that.tmpList.slice(start, end)
           let totalScore = 0
+          that.avg = 0
           for (let i = 0; i < that.total; i++) {
-            totalScore += that.tmpList[i].ccredit
-            that.avg += that.tmpList[i].ccredit * that.tmpList[i].grade
+            totalScore += that.tmpList[i].credit
+            that.avg += that.tmpList[i].credit * that.tmpList[i].score
           }
+          console.log("测试totalScore：" + totalScore)  // 测试
+          console.log("测试avg：" + that.avg)  // 测试
           if (totalScore === 0)
             that.avg = 0
           else
--- frontend/src/views/Student/selectCourse/selectCourse.vue
+++ frontend/src/views/Student/selectCourse/selectCourse.vue
@@ -4,24 +4,24 @@
       <el-main>
         <el-card>
           <el-form :inline="true" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px" class="demo-ruleForm">
-            <el-form-item label="工号" prop="tid">
+            <el-form-item label="教师号" prop="teacherNumber">
               <el-input v-model.number="ruleForm.tid"></el-input>
             </el-form-item>
-            <el-form-item label="教师名" prop="tname">
+            <el-form-item label="教师ID" prop="teacherID">
               <el-input v-model.number="ruleForm.tname"></el-input>
             </el-form-item>
-            <el-form-item label="教师模糊查询">
+            <!-- <el-form-item label="教师模糊查询">
               <el-switch v-model="ruleForm.tFuzzy"></el-switch>
-            </el-form-item>
-            <el-form-item label="课程号" prop="cid">
+            </el-form-item> -->
+            <el-form-item label="课程号" prop="courseNumber">
               <el-input v-model.number="ruleForm.cid"></el-input>
             </el-form-item>
-            <el-form-item label="课程名" prop="cname">
+            <el-form-item label="课程ID" prop="courseID">
               <el-input v-model.number="ruleForm.cname"></el-input>
             </el-form-item>
-            <el-form-item label="课程模糊查询">
+            <!-- <el-form-item label="课程模糊查询">
               <el-switch v-model="ruleForm.cFuzzy"></el-switch>
-            </el-form-item>
+            </el-form-item> -->
             <el-form-item>
               <el-button type="primary" @click="resetForm('ruleForm')">重置</el-button>
             </el-form-item>
@@ -41,21 +41,21 @@ export default {
   data() {
     return {
       ruleForm: {
-        tid: null,
-        cid: null,
-        cname: null,
-        tname: null,
-        tFuzzy: true,
-        cFuzzy: true
+        teacherNumber: null,
+        teacherID: null,
+        courseNumber: null,
+        courseID: null,
+        // tFuzzy: true,
+        // cFuzzy: true
       },
-      rules: {
-        tid: [
-          { type: 'number', message: '必须是数字类型' }
-        ],
-        cid: [
-          { type: 'number', message: '必须是数字类型' }
-        ],
-      }
+      // rules: {
+      //   tid: [
+      //     { type: 'number', message: '必须是数字类型' }
+      //   ],
+      //   cid: [
+      //     { type: 'number', message: '必须是数字类型' }
+      //   ],
+      // }
     };
   },
   methods: {
